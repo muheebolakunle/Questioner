be50e560ad9ed95ee41b0f7f7ad204de5db1ebe2
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2019-01-03 15:29:10 +0100

    Modify files to implement feature

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import meetupRoute from './meetuproute';
+import questionRoute from './questionroute';
 import appRoute from './approute';
 
 const router = new Router();
 
 router.use('/meetups', meetupRoute);
+router.use('/questions', questionRoute);
 router.use('/*', appRoute);
 
 export default router;
