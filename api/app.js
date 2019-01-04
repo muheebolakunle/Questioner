5fd7e3f17bcb1ec65d53164046e947d9624d57de
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2019-01-04 21:33:56 +0100

    Modify undefined routes

--- api/app.js
+++ api/app.js
@@ -8,6 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1', routes);
 
+app.all('/*', (req, res) => res.status(404).send({ message: 'Invalid request.' }));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -7,6 +7,6 @@ const router = new Router();
 
 router.use('/meetups', meetupRoute);
 router.use('/questions', questionRoute);
-router.use('/*', appRoute);
+router.use('/', appRoute);
 
 export default router;
