aed941237f4e9d56f314ed803c62c9a6f17ef3f6
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2019-01-04 09:52:45 +0100

    Modify files to implement feature

--- api/v1/routes/meetuproute.js
+++ api/v1/routes/meetuproute.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import meetupController from '../controllers/meetupcontroller';
+import rsvpController from '../controllers/rsvpcontroller';
 import validateMeetup from '../middleware/validatemeetup';
+import validateRsvp from '../middleware/validatersvp';
 import idValidator from '../middleware/idvalidator';
 
 const {
   createMeetup, getOneMeetup, getAllMeetups, getUpcomingMeetups,
 } = meetupController;
+const { respond } = rsvpController;
 
 const router = express.Router();
 
 router.post('/', validateMeetup, createMeetup);
+router.post('/:id/rsvps', idValidator, validateRsvp, respond);
 router.get('/', getAllMeetups);
 router.get('/upcoming', getUpcomingMeetups);
 router.get('/:id', idValidator, getOneMeetup);
